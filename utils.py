e7b400096affc2139db44d23cdf7c39bdf20d2b8
MrSemyon12 <denisenko.box19@gmail.com>
2023-11-16 20:24:52 +1000

    update email regex

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from db import FieldType
 
 DATE_REGEX = re.compile(r"^(\d{2}\.\d{2}\.\d{4}|\d{4}-\d{2}-\d{2})$")
 PHONE_REGEX = re.compile(r"^\+7 \d{3} \d{3} \d{2} \d{2}$")
-EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+$")
 
 
 def parse_form_data(form_data: str) -> list[tuple[str, str]]:
